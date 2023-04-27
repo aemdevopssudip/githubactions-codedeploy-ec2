1fed1516a4e9809412d23e863a4262c587494684
aemdevopssudip <102893066+aemdevopssudip@users.noreply.github.com>
2023-04-27 18:17:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 
-const version = 'v:33';
+const version = 'v:44';
 
 app.get('/', (req, res) => {
     // set response content    
