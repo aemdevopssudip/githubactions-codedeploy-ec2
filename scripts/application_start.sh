5685078593cbf86ffd26da8d7fc88a1789ebae75
aemdevopssudip <102893066+aemdevopssudip@users.noreply.github.com>
2023-04-27 16:16:10 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/githubactions-codedeploy-ec2/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/githubactions-codedeploy-ec2/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/githubactions-codedeploy-ec2/deploy.log
+echo 'pm2 restart mynode-app' >> /home/ec2-user/githubactions-codedeploy-ec2/deploy.log
+pm2 restart mynode-app >> /home/ec2-user/githubactions-codedeploy-ec2/deploy.log
