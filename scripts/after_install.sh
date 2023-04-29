8d7a48a2cf96e8238c83d6d2e4d3884cc548f1bd
aemdevopssudip <102893066+aemdevopssudip@users.noreply.github.com>
2023-04-29 18:17:00 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,6 @@
 sudo sh -c "echo 'run after_install.sh: ' >> /home/ubuntu/githubactions-codedeploy-ec2/deploy.log"
 
 sudo sh -c "echo 'cd /home/ubuntu/githubactions-codedeploy-ec2' >> /home/ubuntu/githubactions-codedeploy-ec2/deploy.log"
-sudo sh -c "cd /home/ubuntu/githubactions-codedeploy-ec2 >> /home/ubuntu/githubactions-codedeploy-ec2/deploy.log"
-
+cd /home/ubuntu/githubactions-codedeploy-ec2
 sudo sh -c "echo 'npm install' >> /home/ubuntu/githubactions-codedeploy-ec2/deploy.log" 
-sudo npm install #>> /home/ubuntu/githubactions-codedeploy-ec2/deploy.log"
+sudo npm install >> /home/ubuntu/githubactions-codedeploy-ec2/deploy.log"
