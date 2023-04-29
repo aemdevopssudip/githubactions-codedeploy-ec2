a5abadc391bdade5bf187be1914ac5eb122db0f5
aemdevopssudip <102893066+aemdevopssudip@users.noreply.github.com>
2023-04-29 18:27:14 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,3 +4,7 @@ sudo sh -c "echo 'run application_start.sh: ' >> /home/ubuntu/githubactions-code
 
 sudo sh -c "echo 'pm2 restart mynode-app' >> /home/ubuntu/githubactions-codedeploy-ec2/deploy.log"
 sudo sh -c "sudo pm2 restart mynode-app >> /home/ubuntu/githubactions-codedeploy-ec2/deploy.log"
+
+#upper command are written such as, so that it will create a deploy.log file in /home/ubuntu/githubactions-codedeploy-ec2 and store the logs line by line. To have more simplified command you can write just this
+
+# sudo pm2 restart mynode-app
