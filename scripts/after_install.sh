23dd0d73fbcf0d7d1a74b16c3cb5bf3f6ae1addd
aemdevopssudip <102893066+aemdevopssudip@users.noreply.github.com>
2023-04-29 18:26:17 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,3 +5,9 @@ sudo sh -c "echo 'cd /home/ubuntu/githubactions-codedeploy-ec2' >> /home/ubuntu/
 cd /home/ubuntu/githubactions-codedeploy-ec2
 sudo sh -c "echo 'npm install' >> /home/ubuntu/githubactions-codedeploy-ec2/deploy.log" 
 sudo sh -c "sudo npm install >> /home/ubuntu/githubactions-codedeploy-ec2/deploy.log"
+
+
+#upper command are written such as, so that it will create a deploy.log file in /home/ubuntu/githubactions-codedeploy-ec2 and store the logs line by line. To have more simplified command you can write just this
+
+# cd /home/ubuntu/githubactions-codedeploy-ec2
+# sudo npm install
